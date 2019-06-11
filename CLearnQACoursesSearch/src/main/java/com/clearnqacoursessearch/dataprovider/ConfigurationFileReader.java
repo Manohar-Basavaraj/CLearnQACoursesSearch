1129b662c74261627cd4266f90a47a53d0dc499c
manohar.b <manohar.b@SPICITYL542.SPI.COM>
2019-06-11 17:17:39 +0530

    Login done

--- CLearnQACoursesSearch/src/main/java/com/clearnqacoursessearch/dataprovider/ConfigurationFileReader.java
+++ CLearnQACoursesSearch/src/main/java/com/clearnqacoursessearch/dataprovider/ConfigurationFileReader.java
@@ -1,7 +1,31 @@
 package com.clearnqacoursessearch.dataprovider;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationFileReader {
 	
+	private Properties properties;
+	private final String propertyFilePath= "Configuration//Configuration.properties";
 	
+	public void ConfigFileReader(){
+		 BufferedReader reader;
+		 try {
+		 reader = new BufferedReader(new FileReader(propertyFilePath));
+		 properties = new Properties();
+		 try {
+		 properties.load(reader);
+		 reader.close();
+		 } catch (IOException e) {
+		 e.printStackTrace();
+		 }
+		 } catch (FileNotFoundException e) {
+		 e.printStackTrace();
+		 throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+		 } 
+		 }
 
 }
